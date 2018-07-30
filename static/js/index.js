855799cf36b6890d408c8f411d915f0509cd9a54
mangonob <mangonob@qq.com>
2018-07-31 01:48:12 +0800

    Initial Commit

---
+++ static/js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(Ready)
+
+function Ready () {
+
+  function imgReload()
+  {
+    var imgHeight = 0;
+    var wtmp = $("body").width();
+    $("#b06 ul li").each(function(){
+      $(this).css({width:wtmp + "px"});
+    });
+    $(".sliderimg").each(function(){
+      $(this).css({width:wtmp + "px"});
+      imgHeight = $(this).height();
+    });
+  }
+  imgReload();
+  var unslider06 = $('#b06').unslider({
+      dots: true,
+      fluid: true
+    }),
+    data06 = unslider06.data('unslider');
+
+  $('.unslider-arrow06').click(function() {
+    var fn = this.className.split(' ')[1];
+    data06[fn]();
+  });
+
+
+
+
+}
